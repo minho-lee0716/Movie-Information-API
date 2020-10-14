591be706b9862f1554da2a81415d514c716d481c
Min-Ho Lee <minho.lee0716@gmail.com>
2020-10-14 23:58:27 +0900

    Add: Movie Model2

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '^0%fnk0f^*in--ym#7=*k##ikuvqz7_da10w2b##80#4+wbzr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    movie_id = models.AutoField(primary_key=True)
+
+    class Meta:
+        db_table='movies'
