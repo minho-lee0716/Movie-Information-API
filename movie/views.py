89931abbbebd052f048a5aaf46b685d045977bd5
Min-Ho Lee <minho.lee0716@gmail.com>
2020-10-16 19:14:51 +0900

    [ Database ] - AWS RDS 사용(MySQL) - privacy 파일 추가(SECRET_KEY, DATABASES 등의 정보가 들어있음)
    [ .gitignore ]
    - SECERT_KEY, DATABASES 정보가 들어있는 privacy 파일 추가
    
    [ Test EndPoint ]
    - EndPoint Test
    
    [ SQLite3 ]
    - AWS RDS(MySQL) 사용으로 인한 db.sqlite3 파일 삭제

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+class MovieView(View):
+    def get(self, request):
+        return JsonResponse({'message':'SUCCESS_TEST'}, status=200)
