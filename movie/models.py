2c2edf8943900d12111c9b34b4047014d3a535c4
Min-Ho Lee <minho.lee0716@gmail.com>
2020-10-16 19:58:58 +0900

    [ Movie App ] - models.py > makemigration & migrate - rename movies.desc_text to movies.introduction

--- movie/models.py
+++ movie/models.py
@@ -8,7 +8,7 @@ class Movie(models.Model):
     openning_date     = models.DateTimeField(auto_now_add=True)
     grade             = models.DecimalField(max_digits=4, decimal_places=2)
     running_time      = models.IntegerField()
-    desc_text         = models.TextField(max_length=2000)
+    introduction      = models.TextField(max_length=2000)
     number_of_viewers = models.IntegerField(default=0)
 
     class Meta:
